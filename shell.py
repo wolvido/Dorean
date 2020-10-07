647f5402e901d98b123b64d4e7c759a3040aa668
winzyl <prowinzil@gmail.com>
2020-10-08 02:33:01 +0800

    added lists

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import MyLang
 
 while True:
-    text = input('MyLang > ')
-    result, error = MyLang.run('<stdin>', text)
+	text = input('MyLang > ')
+	result, error = MyLang.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error: print(error.as_string())
+	else: print(result)
